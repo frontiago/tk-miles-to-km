1b765d6d461d96d3bca590d7b870fbda46c266ae
Thiago Silva <thiagowfer@gmail.com>
2024-02-22 22:07:51 -0300

    organize the code and widgets

--- tk-miles-to-km.py
+++ tk-miles-to-km.py
@@ -1,16 +1,16 @@
-# Tkinter - Miles to kilometer converter
+# Tkinter - Miles to kilometers conversion
 
 import tkinter as tk 
 # from tkinter import ttk 
-import ttkbootstrap as ttk 
+import ttkbootstrap as ttk
 
 def convert():
     result = entry_int.get() * 1.61
     output_string.set(f'{result:.2f} kms')
 
-# window 
+# window
 window = tk.Tk()
-window.title('Miles to KM converter')
+window.title('LEARNING TKINTER')
 window.geometry('300x200')
 
 # title 
@@ -19,21 +19,20 @@ title_label.pack(pady = 15)
 
 # input field 
 input_frame = ttk.Frame(master = window)
+input_frame.pack(pady = 10)
+
+# entry
 entry_int = tk.IntVar()
 entry = ttk.Entry(master = input_frame, textvariable = entry_int)
-button = ttk.Button(master = input_frame, text = 'Convert', command = convert)
-
-input_frame.pack(pady = 10)
 entry.pack(side = 'left', padx = 10)
+
+# button
+button = ttk.Button(master = input_frame, text = 'Convert', command = convert)
 button.pack(side = 'left')
 
 # output 
 output_string = tk.StringVar()
-output_label = tk.Label(
-    master = window, 
-    text = 'Output',
-    font = 'Calibri 22',
-    textvariable = output_string) 
+output_label = ttk.Label(master = window, text = 'Output', font = 'Calibri 22', textvariable = output_string)
 output_label.pack(pady = 5)
 
 # run
